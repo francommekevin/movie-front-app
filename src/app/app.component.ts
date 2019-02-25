b4d984e5691df25c17c1328c4270e8fa9385e3c4
RomainTellier <r0oma1n.tellier@gmail.com>
2019-02-25 14:42:22 +0100

    Add menu custom (responsive too)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { FocusMonitor } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  constructor(private _focusMonitor: FocusMonitor) {
+  }
+
+  ngAfterViewInit() {
+    this._focusMonitor.stopMonitoring(document.getElementById('stop-cdk-nav'));
+  }
 }
